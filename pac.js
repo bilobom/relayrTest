10117a9c21670cb383aaa10b6a598df84522cb59
bilobom <bilobom124@gmail.com>
2021-01-28 11:50:49 +0100

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,8 +1,7 @@
 function FindProxyForURL(url, host) {
 
     // use proxy for specific domains
-    if (shExpMatch(host, "*://*.hsoubservices.com/*"))
-        return "PROXY 127.0.0.1:8080";
+   
 
     // by default use no proxy
     return "DIRECT";
